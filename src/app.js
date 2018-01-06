946a496db250d094ede9ef6f782dbe4a971c8aae
vishalcoderathore <vshl.rathore87@gmail.com>
2018-01-06 11:23:51 -0600

    Setup devDependancies and dist folder

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import 'react-dates/lib/css/_datepicker.css';
 
 //Create Store
 const store = configureStore();
-console.log('test');
+
 const jsx = (
     <Provider store={store}>
         <AppRouter/>
