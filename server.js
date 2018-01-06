a360dfc2f07eefdb0a66641aaa66dd0de7a724f5
vishalcoderathore <vshl.rathore87@gmail.com>
2018-01-06 01:36:51 -0600

    Setup Heroku producion server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
+const express = require('express');
+const path = require('path');
 
 //Create our App
 var app = express();
 const PORT = process.env.PORT || 8080;
+const publicPath = path.join(__dirname, 'public');
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
@@ -15,7 +17,11 @@ app.use(function (req, res, next){
 
 //use() lets to add functionality to our express app
 //static requires a folder location, in our case its public
-app.use(express.static('public'));
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 //start the server
 app.listen(PORT, function() {
