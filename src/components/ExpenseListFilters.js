ec12c5b704aaaf05150ffce4b81ee2a67c021217
vishalcoderathore <vshl.rathore87@gmail.com>
2018-01-14 11:56:52 -0600

    Added Filters Section CSS

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { DateRangePicker } from 'react-dates';
 import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters';
 
-export class ExpenseListFilters extends React.Component{
+export class ExpenseListFilters extends React.Component {
     state = {
-        calenderFocused : null
+        calenderFocused: null
     };
-    
-    onDatesChange = ({ startDate, endDate}) => {
+
+    onDatesChange = ({ startDate, endDate }) => {
         this.props.setStartDate(startDate);
         this.props.setEndDate(endDate);
     };
 
     onFocusChange = (calenderFocused) => {
-        this.setState( () => ({ calenderFocused }));
+        this.setState(() => ({ calenderFocused }));
     }
 
     onTextChange = (e) => {
@@ -22,36 +22,44 @@ export class ExpenseListFilters extends React.Component{
     };
 
     onSortChange = (e) => {
-        if(e.target.value === 'date'){
+        if (e.target.value === 'date') {
             this.props.sortByDate();
         }
-        else if(e.target.value === 'amount'){
+        else if (e.target.value === 'amount') {
             this.props.sortByAmount();
         }
     };
 
-    render(){
+    render() {
         return (
-            <div>
-                <input type='text' value={this.props.filters.text} onChange={this.onTextChange} placeholder="Search"/>
-        
-                <select value = {this.props.filters.sortBy} onChange={this.onSortChange}>
-                    <option value='date'>Date</option>
-                    <option value='amount'>Amount</option>
-                </select>
+            <div className="content-container">
+                <div className="input-group">
+                    <div className="input-group__item">
+                        <input type='text' value={this.props.filters.text} onChange={this.onTextChange} placeholder="Search" />
+                    </div>
 
-                <DateRangePicker 
-                    startDate = {this.props.filters.startDate}
-                    endDate = {this.props.filters.endDate}
-                    onDatesChange = {this.onDatesChange}
-                    focusedInput = {this.state.calenderFocused}
-                    onFocusChange = {this.onFocusChange}
-                    startDateId = 'start'
-                    endDateId = 'end'
-                    numberOfMonths = {1}
-                    isOutsideRange = {() => false}
-                    showClearDates = {true}
-                />
+                    <div className="input-group__item">
+                        <select value={this.props.filters.sortBy} onChange={this.onSortChange}>
+                            <option value='date'>Date</option>
+                            <option value='amount'>Amount</option>
+                        </select>
+                    </div>
+
+                    <div className="input-group__item">
+                        <DateRangePicker
+                            startDate={this.props.filters.startDate}
+                            endDate={this.props.filters.endDate}
+                            onDatesChange={this.onDatesChange}
+                            focusedInput={this.state.calenderFocused}
+                            onFocusChange={this.onFocusChange}
+                            startDateId='start'
+                            endDateId='end'
+                            numberOfMonths={1}
+                            isOutsideRange={() => false}
+                            showClearDates={true}
+                        />
+                    </div>
+                </div>
             </div>
         )
     }
@@ -65,11 +73,11 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        setTextFilter : (text) => dispatch(setTextFilter(text)),
-        sortByDate : () => dispatch(sortByDate()),
+        setTextFilter: (text) => dispatch(setTextFilter(text)),
+        sortByDate: () => dispatch(sortByDate()),
         sortByAmount: () => dispatch(sortByAmount()),
-        setStartDate : (startDate) => dispatch(setStartDate(startDate)),
-        setEndDate : (endDate) => dispatch(setEndDate(endDate))
+        setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+        setEndDate: (endDate) => dispatch(setEndDate(endDate))
     }
 }
 
