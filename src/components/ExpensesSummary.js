37f6a23f454171b00c2b4ca9d96143ae4402634c
vishalcoderathore <vshl.rathore87@gmail.com>
2018-01-14 10:54:01 -0600

    Added Summary Area CSS with updated test snapshots

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
+import { Link } from 'react-router-dom';
 import expenses_total from '../selectors/expenses-total';
 import getVisibleExpenses from '../selectors/visibleExpenses';
 
-export const ExpensesSummary = ({expenseCount, expenseTotal}) => {
+export const ExpensesSummary = ({ expenseCount, expenseTotal }) => {
     const formattedExpenseTotal = numeral(expenseTotal).format('$0,0.00');
-    return(
-        <div>
-            <h1>Viewing {expenseCount} {expenseCount ===1 ? 'expense' : 'expenses'} toatalling {formattedExpenseTotal}</h1>
+    return (
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseCount === 1 ? 'expense' : 'expenses'} totalling <span>{formattedExpenseTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     );
 };
