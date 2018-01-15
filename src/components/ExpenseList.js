6fd086bc3c1654ee805fe0266fd8ee3f426cee09
vishalcoderathore <vshl.rathore87@gmail.com>
2018-01-15 14:44:42 -0600

    Added Expense List CSS

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,18 +4,29 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/visibleExpenses';
 
 export const ExpenseList = (props) => (
-    <div>
-        {
-            props.expenses.length === 0 ? <p>No expenses Added</p> : 
-                props.expenses.map((expense) => {
-                    return <ExpenseListItem  key={expense.id} {...expense}/>
-                })
-        }
+    <div className="content-container">
+        <div className="list-header">
+            <div className="show-for-mobile">Exenses</div>
+            <div className="show-for-desktop">Expense</div>
+            <div className="show-for-desktop">Amount</div>
+        </div>
+        <div className="list-body">
+            {
+                props.expenses.length === 0 ?
+                    <div className="list-item list-item--message">
+                        <span>No expenses Added</span>
+                    </div>
+                    :
+                    props.expenses.map((expense) => {
+                        return <ExpenseListItem key={expense.id} {...expense} />
+                    })
+            }
+        </div>
     </div>
 );
 
 const mapStateToProps = (state) => {
-    return{
+    return {
         expenses: selectExpenses(state.expenses, state.filters)
     };
 };
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,21 +3,17 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
 
-const ExpenseListItem = ({description, amount, createdAt, note, dispatch, id}) => (
-    <div>
-        
-        <Link to={`/edit/${id}`}>
-            <h3>Description: {description}</h3>
-        </Link>
-        <p>
-            Amount: {numeral(amount).format('$0,0.00')}
-            -
-            Date: {moment(createdAt).format('MMMM Do, YYYY')}
-        </p>
-        <i>Note: {note ? note : 'N/A'}</i>
-       
-    </div>
-        
+const ExpenseListItem = ({ description, amount, createdAt, note, dispatch, id }) => (
+
+    <Link className="list-item" to={`/edit/${id}`}>
+        <div>
+            <h3 className="list-item__title">{description}</h3>
+            <span className="list-item__sub-title">{moment(createdAt).format('MMMM Do, YYYY')}</span>
+        </div>
+        <div>
+            <h3 className="list-item__title">{numeral(amount).format('$0,0.00')}</h3>
+        </div>
+    </Link>
 );
 export default ExpenseListItem;
 
