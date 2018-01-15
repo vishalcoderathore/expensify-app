bdfd719712e57f04145a8b475213654a4e9d6bbb
vishalcoderathore <vshl.rathore87@gmail.com>
2018-01-15 15:16:29 -0600

    Added Loader gif on loading pages

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingPage = () => (
+    <div className="loader">
+        <img className="loader__image" src="/images/lg.dual-gear-loading-icon.gif"/>
+    </div>
+);
+
+export default LoadingPage;
