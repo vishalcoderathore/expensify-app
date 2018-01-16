8214f479c91aee4c0445e7acc169ab7f667b34b7
vishalcoderathore <vshl.rathore87@gmail.com>
2018-01-16 00:32:38 -0600

    Updated Login Page <h1> class

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,7 +5,7 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
-            <h1>Expensify</h1>
+            <h1 className="box-layout__title">Expensify</h1>
             <p>Track your daily expenses at one place</p>
             <button className="button" onClick={startLogin}>Login with Google</button>
         </div>
